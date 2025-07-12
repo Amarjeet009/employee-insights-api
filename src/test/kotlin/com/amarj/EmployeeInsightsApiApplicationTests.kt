1e624c99342b6aa2ec95fa768f24423aecf3c4e7
Amarjeet Rao <amarjeetrao@outlook.com>
2025-07-12 19:10:00 +0530

    First Commit
    Developing an engaging employee management system with Kotlin, leveraging PostgreSQL, MongoDB, and reactive programming through Spring WebFlux for advanced backend architecture.

---
+++ src/test/kotlin/com/amarj/EmployeeInsightsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amarj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeInsightsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
